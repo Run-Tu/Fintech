e770b1a11e14df840f840811ba0c1d7a35f4ed22
pyg <pza07620041@126.com>
2021-12-13 09:01:20 +0800

    process excel xlsx file

---
+++ Statistics/Statistics.py
@@ -0,0 +1,34 @@
+from utils import (
+    get_target_list,
+    save_modify_sheet
+)
+import os
+"""
+    openpyxl教程：https://blog.csdn.net/David_Dai_1108/article/details/78702032
+    Sheet操作：
+        1.加载excel文件(默认可读写);通过参数data_only只显示表格结果数字,过滤公式
+        wb = load_workbook(data_list[0], data_only=True)
+        2.通过get_sheet_names()获得当前xlsx文件的所有sheet
+        name_list = wb.get_sheet_names()
+        3.根据sheet名字获得sheet内容
+        my_sheet = wb.get_sheet_by_name('Sheet1')
+    Cell操作：
+        4.获取某个单元格的值,先字母再数字,先列再行
+        target = my_sheet['B30']
+"""
+# Get All File Name in Specified Path
+data_path_list = ['../xlsx_data/'+data_path for data_path in os.listdir('../xlsx_data')]
+# Find Target Value In Sheet           
+Date, Total_Load_Size, Mid_To_Long_Term_Total_Load_Size,\
+Corporate_Loan_Size, Mid_To_Long_Term_Corporate_Loan_Size,\
+Mid_Loan_Scale, Mid_Corporate_Loan_Scale = get_target_list(data_path_list)
+row_index_list = [row for row in range(5,len(Total_Load_Size)+5)]
+row_index_and_Required_Data = list(zip(row_index_list,Date,Total_Load_Size,Mid_To_Long_Term_Total_Load_Size,
+                                       Mid_Loan_Scale,Corporate_Loan_Size,
+                                       Mid_To_Long_Term_Corporate_Loan_Size,Mid_Corporate_Loan_Scale))
+
+# Save Result
+save_modify_sheet(final_sheet_path='../17·21中长期贷款汇总.xlsx',
+                  data_path='../17·21中长期贷款汇总.xlsx',
+                  required_data=row_index_and_Required_Data
+                  )
---
+++ Statistics/utils.py
@@ -0,0 +1,87 @@
+from typing import List
+from openpyxl import load_workbook
+from openpyxl.cell import MergedCell
+
+
+def get_sheet_object(data_path, sheet='Sheet1', if_data_only=True):
+    """       
+        Pack the function of obtaining sheet objects
+    """
+    wb = load_workbook(data_path, data_only=if_data_only)
+    Sheet = wb.get_sheet_by_name(sheet) 
+
+    return Sheet
+
+
+def save_modify_sheet(final_sheet_path,data_path,required_data:List,sheet='Sheet1'):
+    """
+        Modify the final sheet by target value and save it
+    """
+    wb = load_workbook(final_sheet_path, data_only=True)
+    Final_sheet = wb.get_sheet_by_name(sheet) 
+    for data_tuple in required_data:
+        for col in range(1,len(data_tuple)):
+            Final_sheet.cell(row=data_tuple[0],column=col).value = data_tuple[col]
+    
+    wb.save(data_path)
+
+
+def parser_merged_cell(sheet, row, col):
+    """
+        检查是否为合并单元格并获取对应行列单元格的值。
+        如果是合并单元格，则取合并区域左上角单元格的值作为当前单元格的值,否则直接返回该单元格的值
+        :param sheet: 当前工作表对象
+        :param row: 需要获取的单元格所在行
+        :param col: 需要获取的单元格所在列
+        :return: 
+    """
+    cell = sheet.cell(row=row, column=col) # 定位到某一单元格
+    if isinstance(cell, MergedCell): # 判断该单元格是否为合并单元格
+        for merged_range in sheet.merged_cell_ranges:
+            if cell.coordinate in merged_range:
+                # 获取合并区域左上角的单元格作为该单元格的值返回
+                cell = sheet.cell(row=merged_range.min_row, column=merged_range.min_col)
+                break
+    cell = cell.value.replace(' ','')
+
+    return cell
+
+
+def get_target_list(data_path_list):
+    """
+        Get Date;
+        Total_Load_Size;
+        Mid_To_Long_Term_Total_Load_Size;
+        Corporate_Loan_Scale;
+        Mid_To_Long_Term_Corporate_Loan_Scale;
+        Mid_Loan_Scale;
+        Mid_Corporate_Loan_Scale
+    """
+    Date = []
+    Total_Load_Size = []
+    Mid_To_Long_Term_Total_Load_Size = []
+    Corporate_Loan_Size = []
+    Mid_To_Long_Term_Corporate_Loan_Size = []
+    for data_path in data_path_list:
+        current_sheet = get_sheet_object(data_path)
+        date = parser_merged_cell(current_sheet,2,1)
+        Total_Load_Size_target = current_sheet['D30'].value
+        Mid_To_Long_Term_Total_Load_Size_target = current_sheet['M30'].value
+        Corporate_Loan_Scale_target = current_sheet['B30'].value
+        Mid_To_Long_Term_Corporate_Loan_Scale_target = current_sheet['H30'].value
+        # Collection
+        Date.append(date)
+        Total_Load_Size.append(Total_Load_Size_target)
+        Mid_To_Long_Term_Total_Load_Size.append(Mid_To_Long_Term_Total_Load_Size_target)
+        Corporate_Loan_Size.append(Corporate_Loan_Scale_target)
+        Mid_To_Long_Term_Corporate_Loan_Size.append(Mid_To_Long_Term_Corporate_Loan_Scale_target)
+        Mid_Loan_Scale = ['{:%}'.format(mid_size/total_size) 
+                           for mid_size,total_size 
+                           in zip(Mid_To_Long_Term_Total_Load_Size,Total_Load_Size)]
+        Mid_Corporate_Loan_Scale = ['{:%}'.format(mid_com_size/total_com_size) 
+                                    for mid_com_size,total_com_size
+                                    in zip(Mid_To_Long_Term_Corporate_Loan_Size,Corporate_Loan_Size)]
+
+    return Date,Total_Load_Size, Mid_To_Long_Term_Total_Load_Size, \
+           Corporate_Loan_Size, Mid_To_Long_Term_Corporate_Loan_Size,\
+           Mid_Loan_Scale, Mid_Corporate_Loan_Scale
